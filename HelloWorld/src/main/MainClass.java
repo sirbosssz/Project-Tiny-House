7002208493aa9098eb5b2e975a052302792051c9
Boss'sr ShadowZ <spectreb.boss@gmail.com>
2016-10-10 01:54:22 +0700

    +import test java project: HelloWorld

---
+++ HelloWorld/src/main/MainClass.java
@@ -0,0 +1,15 @@
+package main;
+import javax.swing.*;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		
+		JLabel a = new JLabel("Hello World");
+		JOptionPane.showMessageDialog(a, a);
+		
+
+
+	}
+
+}
